2510c8cbc37d2d9edfd3ece8517216821c450807
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-06-26 17:49:20 +0900

    [Passed] 달에서 무게 구하기 (99ms, 9MB)

---
+++ trail1/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int w = 13;
+        double g = 0.165;
+        System.out.printf(w+" * %.6f = " + "%.6f", g, w*g);
+    }
+}
