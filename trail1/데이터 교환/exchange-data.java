47d3428a6c7519ae492629e7c6fb884e943aff1e
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-06-27 18:36:05 +0900

    [Passed] 데이터 교환 (95ms, 9MB)

---
+++ trail1/데이터 교환/exchange-data.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 6, c = 7;
+        int temp = 0;
+
+        temp = c;
+        c = b;
+        b = a;
+        a = temp;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
