8d3ff8a3c784e6c202ff8a208d9f5e034fc77dfe
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-06-29 18:04:07 +0900

    [Passed] 날짜 변경하여 출력 2 (129ms, 10MB)

---
+++ trail1/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String d = sc.next();
+
+        String[] arr = d.split("-");
+
+        System.out.printf("%s.%s.%s", arr[2], arr[0], arr[1]);
+
+    }
+}
