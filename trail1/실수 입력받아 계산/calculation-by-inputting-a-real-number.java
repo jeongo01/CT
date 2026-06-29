b658ddcfde92c55304fbe0ca2ea8815ca850eaef
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-06-29 16:12:23 +0900

    [Passed] 실수 입력받아 계산 (151ms, 12MB)

---
+++ trail1/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double sum;
+        sum = a+b;
+        System.out.printf("%.2f", sum);
+
+
+    }
+}
