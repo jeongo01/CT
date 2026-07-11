b7bed16458efeed5c8b7d207b280bfc78c3a4243
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-07-11 22:16:34 +0900

    [Passed] 증가시키며 출력하기 (92ms, 9MB)

---
+++ trail1/증가시키며 출력하기/increase-and-print.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 17;
+        for(int i = 5; i <= n; i++) {
+            System.out.print(i + " ");
+            i++;
+        }
+        
+
+    }
+}
