e91916c211fab011c58ff0b3b9cc75b46cf84818
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-07-12 13:21:24 +0900

    [Passed] 감소시키며 출력하기 (93ms, 9MB)

---
+++ trail1/감소시키며 출력하기/decrease-and-print.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 17;
+        
+        for(int i = n; i >= 5; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
