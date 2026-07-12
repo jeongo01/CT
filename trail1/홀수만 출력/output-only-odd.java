3424e3419a8d50dcdf293e5cf7a7c0a2607dfb80
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-07-12 13:11:19 +0900

    [Passed] 홀수만 출력 (141ms, 16MB)

---
+++ trail1/홀수만 출력/output-only-odd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        for(int i = A; i <= B; i++) {
+            if(i % 2 == 1) {
+                System.out.print(i + " ");
+
+            }
+        }
+
+    }
+}
