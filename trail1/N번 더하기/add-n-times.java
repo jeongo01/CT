7f8b0962e67ff7abd4e47d44630bf2391fa7278a
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-07-18 16:30:09 +0900

    [Passed] N번 더하기 (141ms, 10MB)

---
+++ trail1/N번 더하기/add-n-times.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int N = sc.nextInt();
+
+        for(int i = 1; i <= N; i++) {
+            A += N;
+            System.out.println(A);
+        }
+    }
+}
