2bbe731c98e075e90a00b39255c542798aaa0516
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-07-29 21:32:02 +0900

    [Passed] 주어진 수까지의 곱 (132ms, 10MB)

---
+++ trail1/주어진 수까지의 곱/multiplication-up-to-a-given-number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int prod = 1;
+
+        for(int i = A; i <= B; i++) {
+            prod *= i; 
+        }
+        System.out.print(prod);
+
+    }
+}
