f308ecaeea4cfe6f6098c856e2684b82f0aa0026
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-07-30 15:42:54 +0900

    [Passed] 1까지 나누기 (126ms, 10MB)

---
+++ trail1/1까지 나누기/divide-by-1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int cnt = 0;
+
+        for(int i = 1; i <= 5000; i++) {
+            N /= i;
+            cnt++;
+            if(N <= 1) {
+                break;
+            }
+        }
+        System.out.print(cnt);
+
+    }
+}
