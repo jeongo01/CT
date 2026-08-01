cf9ac79ee24d56c911c93679a04497a56e031ecd
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-08-01 15:40:24 +0900

    [Passed] 몫 출력하기 (134ms, 10MB)

---
+++ trail1/몫 출력하기/print-share.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+
+        while(cnt < 3) {
+            int num = sc.nextInt();
+            if(num % 2 == 1) {
+                continue;
+            } else if(num % 2 == 0) {
+                System.out.println(num / 2);
+                cnt++;
+            }
+        }
+    }
+}
