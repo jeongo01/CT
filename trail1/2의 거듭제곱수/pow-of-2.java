1c442b21b9fd843ea205c5e2b259082d855b712d
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-08-05 13:57:40 +0900

    [Passed] 2의 거듭제곱수 (129ms, 10MB)

---
+++ trail1/2의 거듭제곱수/pow-of-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int cnt = 0;
+
+        while(N > 1) {
+            N /= 2;
+            cnt++;
+        }
+        System.out.print(cnt);
+
+    }
+}
