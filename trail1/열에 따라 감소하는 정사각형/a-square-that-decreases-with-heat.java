c04c5b76717ef0d78acf35c6516bc94961850372
jeongo01 <150231524+jeongo01@users.noreply.github.com>
2026-08-15 14:56:32 +0900

    [Passed] 열에 따라 감소하는 정사각형 (128ms, 10MB)

---
+++ trail1/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = 0; i < N; i++) {
+            for(int j = N; j > 0; j--) {
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
